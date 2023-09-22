3805cddb4e1e25d7f81fefb79b14e2398594c08c
Zawahir <139978657+MuhammadZawahir@users.noreply.github.com>
2023-09-22 16:29:41 +0500

    Exercise 19

---
+++ index.js
@@ -0,0 +1,6 @@
+"use strict";
+//Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+Object.defineProperty(exports, "__esModule", { value: true });
+const Exercise_14_1 = require("../Exercise 14");
+//console.log(peopleInvited);
+console.log(`\nI invited ${Exercise_14_1.guests.length} people to the dinner.`);
---
+++ index.ts
@@ -0,0 +1,7 @@
+//Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+
+
+import {guests} from "../Exercise 14"
+//console.log(peopleInvited);
+console.log(`\nI invited ${guests.length} people to the dinner.`);
+
